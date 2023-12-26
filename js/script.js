3b304c5255b5c8c81de2652c1fdf3a3d2c478495
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-12-26 12:45:51 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,27 @@ console.log(a);
 
 console.log("JavaScript is awesome".length);
 console.log("JavaScript is awesome".toUpperCase());
+
+// Const and let
+const yearOfBirth = 1993;
+console.log(yearOfBirth);
+
+let age;
+console.log(age);
+
+age = 14;
+console.log(age);
+
+// constants and CONSTANTS (UPPER_SNAKE_CASE)
+const COLOR_TEAL = "#009688";
+console.log(COLOR_TEAL);
+
+const LOGIN_SUCCESS_MESSAGE = "You are welcome!";
+console.log(LOGIN_SUCCESS_MESSAGE);
+
+let username;
+console.log(username);
+
+let myAge = 30;
+myAge = 25;
+console.log(myAge);
