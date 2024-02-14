b673a316bf9d0372f72a9ba50b6b2b9b5bdb8a28
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-14 10:11:00 +0200

    Update 02-foreach.js

---
+++ src/js/02-foreach.js
@@ -0,0 +1,12 @@
+// * METHOD FOREACH
+const numbers = [5, 10, 15, 20, 25];
+
+// Класичний for
+for (let i = 0; i < numbers.length; i += 1) {
+  console.log(`Індекс ${i}, значення ${numbers[i]}`);
+}
+
+// Перебираючий forEach
+numbers.forEach(function (number, index) {
+  console.log(`Індекс ${index}, значення ${number}`);
+});
