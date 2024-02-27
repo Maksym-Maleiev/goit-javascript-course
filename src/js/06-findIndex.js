897334b2d1bd666bbe9731b239f6cb63201d4470
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-27 13:36:43 +0200

    Create 06-findIndex.js

---
+++ src/js/06-findIndex.js
@@ -0,0 +1,13 @@
+// * METHOD FINDINDEX()
+
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
+
+console.log(colorPickerOptions.findIndex(option => option.label === 'blue')); // 2
+console.log(colorPickerOptions.findIndex(option => option.label === 'pink')); // 3
+console.log(colorPickerOptions.findIndex(option => option.label === 'white')); // -1
